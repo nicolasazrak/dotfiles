11f2fc7dca8ed6dba457f36f0ddd14b51142815e
Nicolas Azrak <azrak.nicolas@gmail.com>
2017-10-08 21:43:06 -0300

    Fixes to install script

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
+# General files
 ln -s $PWD/.bashrc ~/.bashrc
+ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.gitconfig ~/.gitconfig
+
+# VSCode
+mkdir -p  ~/.config/Code/User
 ln -s -b $PWD/vscode/settings.json ~/.config/Code/User/settings.json
 ln -s -b $PWD/vscode/keybindings.json ~/.config/Code/User/keybindings.json
-ln -s $PWD/.vimrc ~/.vimrc
 
 # Fonts
 mkdir -p .config/fontconfig && ln -s $PWD/fonts.conf ~/.config/fontconfig/fonts.conf
 fc-cache -f -v
 
-
-# Zsh
+# ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 mkdir -p ~/.zsh && git clone git@github.com:DFurnes/purer.git ~/.zsh/pure
 mkdir ~/.zfunctions && cp ~/.zsh/pure/pure.zsh ~/.zfunctions/prompt_pure_setup && cp ~/.zsh/pure/async.zsh ~/.zfunctions/async
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
